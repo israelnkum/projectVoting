af55be4453b5dff5c1df90371d1c987763e501b0
israelnkum <israelnkum@gmail.com>
2018-04-09 17:49:25 +0100

    cd

--- validation/voters/uploadVoter.code.php
+++ validation/voters/uploadVoter.code.php
@@ -6,9 +6,6 @@ if (isset($_POST['btn_upload'])){
 
     $fileName = $_FILES['selected_file']['name'];
     $fileTmpName = $_FILES['selected_file']['tmp_name'];
-    $fileSize = $_FILES['selected_file']['size'];
-    $fileError = $_FILES['selected_file']['error'];
-    $fileType= $_FILES['selected_file']['name'];
 
     $fileExt = pathinfo($fileName, PATHINFO_EXTENSION);
 
@@ -17,6 +14,39 @@ if (isset($_POST['btn_upload'])){
     if (!in_array($fileExt,$files_allowed)){
 
         header("Location: ../../pages/voters/uploadVoter.inc.php?fileTypeError=".urlencode("Please Select a .csv File"));
+    }else{
+        $handle = fopen($fileTmpName, 'r');
+        while (($myData = fgetcsv($handle,1000,',')) !== FALSE){
+            $fName = $myData[0];
+            $lName = $myData[1];
+            $oName = $myData[2];
+            $class = $myData[3];
+            $indexNumber = $myData[4];
+            $password = $myData[5];
+
+            $sql = "INSERT INTO voters
+        (firstName, lastName, otherName, class, indexNumber, voterPassword) 
+        VALUES
+        (:firstName,:lastName,:otherName,:class,:indexNumber,:voterPassword )";
+
+            $stmt = $connection -> prepare($sql);
+            $stmt->bindValue(':firstName',$fName);
+            $stmt->bindValue(':lastName',$lName);
+            $stmt->bindValue(':otherName',$oName);
+            $stmt->bindValue(':class',$class);
+            $stmt->bindValue(':indexNumber',$indexNumber);
+            $stmt->bindValue(':voterPassword',$password);
+
+
+            $result = $stmt->execute();
+            if($result ===true){
+                header("Location: ../../pages/voters/uploadVoter.inc.php?fileTypeError=".urlencode("Please Select a .csv File"));
+
+            }else {
+                $validator['success'] = false;
+
+            }
+        }
     }
 
 }
